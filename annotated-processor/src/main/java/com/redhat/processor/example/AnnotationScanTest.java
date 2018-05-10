c0f63bd3fcbade51313ecf8321c2af6345400fce
hhiden <hhiden@172.17.130.167>
2018-05-10 11:25:57 +0100

    Created a simple application to search for message handlers and start them

---
+++ annotated-processor/src/main/java/com/redhat/processor/example/AnnotationScanTest.java
@@ -0,0 +1,16 @@
+
+package com.redhat.processor.example;
+
+import com.redhat.processor.annotations.MessageProcessor;
+import eu.infomas.annotation.AnnotationDetector;
+import java.lang.annotation.Annotation;
+
+/**
+ *
+ * @author hhiden
+ */
+public class AnnotationScanTest {
+    public static void main(String[] args){
+
+    }
+}
